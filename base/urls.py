3756a88cddaff0ebc7937ecca206c8cf345412d1
sdulam-05 <sdulam@charlotte.edu>
2025-02-09 19:02:47 -0500

    1:04:25 - Database & Admin Panel

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
-    path('room/', views.room, name='room'),  # Room page
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,23 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Home Page")
+# Sample data for rooms
+rooms = [
+    {'id': 1, 'name': 'Let’s Learn Python!'},
+    {'id': 2, 'name': 'Design with Me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
 
-def room(request):
-    return HttpResponse("Room Page")
+# Home View
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+# Room View (Dynamic)
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
