633377555f1cf221055e80168c6977ebbcb039b3
sdulam-05 <sdulam@charlotte.edu>
2025-02-09 19:28:02 -0500

    2:06:22 - Search

--- base/models.py
+++ base/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Topic Model
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-# Room Model
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
@@ -17,10 +15,12 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
-# Message Model
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
